021bf82b3c3dae930d04e8f8c8f6767c99f7e870
Anduy Tran <anduytran@ufl.edu>
2025-10-22 13:34:44 -0400

    commented out "wrong function" and added return expectations

--- src/messages.py
+++ src/messages.py
@@ -3,17 +3,16 @@ import struct
 
 # ABDUL'S WORK - MIGHT NEED TO BE DELETED 
 
-
 # this part makes the handshake (prob from CLI input?)
-def make_handshake(peer_id: int):
+def make_handshake(peer_id: int) -> bytes:
     header = b'P2PFILESHARINGPROJ'
     zero_bits = bytes(10)
-    peer_id_bytes = peer_id.to_bytes(4, byteorder='big')
+    peer_id_bytes = peer_id.to_bytes(4, byteorder='big') # Does the same thing as struct.pack I think
     return header + zero_bits + peer_id_bytes
 
 # this func verifies that the handshake it recieves is valid
 # i think this is right but idrk
-def verify_handshake(handshake: bytes):
+def verify_handshake(handshake: bytes) -> bool:
     if len(handshake) != 32:
         return False
     header = handshake[0:18]
@@ -27,26 +26,6 @@ def verify_handshake(handshake: bytes):
         return False
     return True
 
-
-# After handshaking, each peer can send a stream of actual messages
-# I THINK THIS IS COMPLETELY WRONG
-def make_actual(payload=b''):
-    length = payload[0:4]
-    type = int(payload[4:5])
-    if not 0 <= type <= 7:
-        return "Must be between 0-7"
-    if 0 <= type <= 3:
-        if len(payload) != 5:
-            return "No payload should be present!"
-    elif type == 4 or type == 6:
-        if len(payload) != 9:
-            return "Incorrect payload size!"
-    elif type == 5:
-        if len(payload) < 6:
-            return "Payload too small!"
-    elif type == 7:
-        if len(payload) < 9:
-            return "Payload too small!"
 
 
 # DYLAN'S WORK STARTS HERE
